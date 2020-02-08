1d09b1138402be0d2733cd642c871fbb41bc3358
mkhorin <maxhorin@ya.ru>
2020-02-08 20:15:14 +1000

    Install asset vendors

---
+++ asset/vendor/install.js
@@ -0,0 +1,8 @@
+/**
+ * @copyright Copyright (c) 2020 Maxim Khorin <maksimovichu@gmail.com>
+ */
+'use strict';
+
+const SystemHelper = require('areto/helper/SystemHelper');
+
+SystemHelper.spawnProcess(__dirname, 'npm', ['install']);
