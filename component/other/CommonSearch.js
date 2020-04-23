1cfd9fbbdbbb179e64beb00495f528618005237b
mkhorin <maxhorin@ya.ru>
2020-04-23 11:44:45 +1000

    Add search by object title

--- component/other/CommonSearch.js
+++ component/other/CommonSearch.js
@@ -7,63 +7,84 @@ const Base = require('areto/base/Base');
 
 module.exports = class CommonSearch extends Base {
 
+    static getConstants () {
+        return {
+            CONDITION_METHODS: this.getConditionMethods()
+        };
+    }
+
+    static getConditionMethods () {
+        return {
+            float: 'getNumberTypeCondition',
+            date: 'getDateTypeCondition',
+            datetime: 'getDateTypeCondition',
+            id: 'getIdTypeCondition',
+            integer: 'getNumberTypeCondition',
+            number: 'getNumberTypeCondition',
+            timestamp: 'getDateTypeCondition',
+            title: 'getTitleTypeCondition'
+        };
+    }
+
     async resolve (query, value) {
-        const conditions = [], ownerMap = {};
+        this.conditions = [];
         this.query = query;
         for (const column of this.columns) {
             if (column.searchable === true) {
-                const condition = this.getConditionByType(column.type, column.name, value);
-                if (condition) {
-                    column.owner
-                        ? ObjectHelper.push(condition, column.owner, ownerMap)
-                        : conditions.push(condition);
-                }
+                await this.resolveCondition(column, value);
             }
         }
-        for (const name of Object.keys(ownerMap)) {
-            conditions.push(await this.resolveOwnerConditions(name, ownerMap[name]));
-        }
-        if (conditions.length) {
-            query.and(['OR', ...conditions]);
+        if (this.conditions.length) {
+            query.and(['OR', ...this.conditions]);
         }
     }
 
-    async resolveOwnerConditions (owner, conditions) {
-        const relation = this.query.model.getRelation(owner);
+    async resolveCondition (column, value) {
+        const method = this[this.CONDITION_METHODS[column.type]] || this.getDefaultTypeCondition;
+        const condition = method.call(this, column.name, value, column);
+        if (!condition) {
+            return null;
+        }
+        if (!column.relation) {
+            return this.conditions.push(condition);
+        }
+        const name = column.relation === true ? column.name : column.relation;
+        const relation = this.query.model.getRelation(name);
         if (!relation) {
-            return this.throwBadRequest(`Relation not found: ${owner}`);
+            return this.throwBadRequest(`Relation not found: ${name}`);
         }
-        const query = relation.model.find(...conditions);
-        // simple relation without via
-        return {[relation.linkKey]: await query.column(relation.refKey)};
+        // relation without via
+        const query = relation.model.find(condition);
+        this.conditions.push({[relation.linkKey]: await query.column(relation.refKey)});
     }
 
-    getConditionByType (type, attr, value) {
-        switch (type) {
-            case 'number':
-            case 'integer':
-            case 'float': {
-                value = Number(value);
-                return isNaN(value) ? null : {[attr]: value};
-            }
-            case 'date':
-            case 'datetime':
-            case 'timestamp': {
-                value = DateHelper.parse(value, this.controller.language);
-                value = DateHelper.getDayInterval(value);
-                return value ? ['AND', ['>=', attr, value[0]], ['<', attr, value[1]]] : null;
-            }
-            case 'id': {
-                value = this.query.getDb().normalizeId(value);
-                return value ? {[attr]: value} : null;
-            }
-        }
+    getDefaultTypeCondition (attr, value) {
         value = EscapeHelper.escapeRegex(value);
         return ['LIKE', attr, new RegExp(value, 'i')];
+    }
+
+    getDateTypeCondition (attr, value) {
+        value = DateHelper.parse(value, this.controller.language);
+        value = DateHelper.getDayInterval(value);
+        return value ? ['AND', ['>=', attr, value[0]], ['<', attr, value[1]]] : null;
+    }
+
+    getIdTypeCondition (attr, value) {
+        value = this.query.getDb().normalizeId(value);
+        return value ? {[attr]: value} : null;
+    }
+
+    getNumberTypeCondition (attr, value) {
+        value = Number(value);
+        return isNaN(value) ? null : {[attr]: value};
+    }
+
+    getTitleTypeCondition (attr, value) {
+        value = new RegExp(EscapeHelper.escapeRegex(value), 'i');
+        return ['OR', ['LIKE', 'name', value], ['LIKE', 'label', value]];
     }
 };
 module.exports.init();
 
 const EscapeHelper = require('areto/helper/EscapeHelper');
 const DateHelper = require('areto/helper/DateHelper');
-const ObjectHelper = require('areto/helper/ObjectHelper');
