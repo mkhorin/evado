fd73b5a7003f180542636e80e6372504d533e76b
mkhorin <maksimovichu@gmail.com>
2021-07-11 08:55:11 +1000

    Tests for the select helper

--- test/run/component/helper/select.js
+++ test/run/component/helper/select.js
@@ -19,4 +19,65 @@ describe('SelectHelper', ()=> {
         ]);
         expect(SelectHelper.getMapItems(null)).to.eql([]);
     });
+
+    it('getLabelText', () => {
+        expect(SelectHelper.getLabelText({
+            label: 'Label',
+            name: 'Name'
+        })).to.eql('Label (Name)');
+        expect(SelectHelper.getLabelText({
+            name: 'Name'
+        })).to.eql('Name');
+    });
+
+    it('getItemText', () => {
+        const params = {
+            textKey: 'text',
+            valueKey: 'value'
+        };
+        expect(SelectHelper.getItemText({
+            text: 'Text',
+            value: 'Value'
+        }, params)).to.eql('Text');
+        expect(SelectHelper.getItemText({
+            text: null,
+            value: 'Value'
+        }, params)).to.eql('Value');
+    });
+
+    it('getItems', () => {
+        let items = [{
+            _id: 1,
+            name: 'n1'
+        }, {
+            _id: 2,
+            name: 'n2'
+        }];
+        expect(SelectHelper.getItems(items)).to.eql([{
+            value: 1,
+            text: 'n1'
+        }, {
+            value: 2,
+            text: 'n2'
+        }]);
+        items = [{
+            key: 1,
+            caption: 'c1'
+        }, {
+            key: 2,
+            caption: 'c2'
+        }];
+        let params = {
+            valueKey: 'key',
+            textKey: 'caption',
+            getItemText: data => `Custom ${data.caption}`
+        };
+        expect(SelectHelper.getItems(items, params)).to.eql([{
+            value: 1,
+            text: 'Custom c1'
+        }, {
+            value: 2,
+            text: 'Custom c2'
+        }]);
+    });
 });
